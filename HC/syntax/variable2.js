2cc349bd7597553a7ed80e39941d9788321a9810
HeonchanKim <khc9274@naver.com>
2022-03-15 22:08:01 +0900

    0315 공부 및 정리

---
+++ HC/syntax/variable2.js
@@ -0,0 +1,3 @@
+var name = 'egoing';
+var letter = 'Dear '+ name +' Lorem ipsum dolor sit ' + name + ' amet, consectetur adipisicing elit, sed do eiusmod ' + name + ' tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
+console.log(letter);
