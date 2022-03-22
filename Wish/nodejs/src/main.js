28e968422801a086f3be331c97066b22c61c1833
김소원 <a90614@naver.com>
2022-03-23 02:42:12 +0900

    ~파일 생성과 리다이렉션 : 소원

--- Wish/nodejs/src/main.js
+++ Wish/nodejs/src/main.js
@@ -1,6 +1,7 @@
 var http = require("http");
 var fs = require("fs");
 var url = require("url");
+var qs = require("querystring");
 
 function templateHTML(title, list, body) {
   return `<!DOCTYPE html>
@@ -12,6 +13,7 @@ function templateHTML(title, list, body) {
     <body>
       <h1><a href="/">WEB</a></h1>
       ${list}
+      <a href="/create">create</a>
       ${body}
     </body>
   </html>
@@ -69,6 +71,43 @@ var app = http.createServer(function (request, response) {
         );
       });
     }
+  } else if (pathname === "/create") {
+    fs.readdir("../data", function (error, filelist) {
+      var title = "WEB - Create";
+      var list = templateList(filelist);
+      var template = templateHTML(
+        title,
+        list,
+        `<form action="http://localhost:3000/create_process" method="post">
+          <p><input type="text" name="title" placeholder="title"/></p>
+          <p>
+            <textarea name="description" placeholder="description"></textarea>
+          </p>
+          <p>
+            <input type="submit" />
+          </p>
+        </form>`
+      );
+
+      response.writeHead(200);
+      response.end(template);
+    });
+  } else if (pathname === "/create_process") {
+    var body = "";
+    request.on("data", function (data) {
+      body += data;
+    });
+    request.on("end", function () {
+      var post = qs.parse(body);
+      var title = post.title;
+      var description = post.description;
+
+      // fs.writeFile(filename, content, encoding, callback)
+      fs.writeFile(`../data/${title}`, description, "utf-8", function (err) {
+        response.writeHead(302, { Location: `/?id=${title}` });
+        response.end();
+      });
+    });
   } else {
     response.writeHead(404);
     response.end("Not found");
