01e8764b4e0382770c8337a53e00f2d3dde6e7e8
김소원 <a90614@naver.com>
2022-03-24 01:06:49 +0900

    ~ 글 삭제 : 소원

--- Wish/nodejs/src/main.js
+++ Wish/nodejs/src/main.js
@@ -3,7 +3,7 @@ var fs = require("fs");
 var url = require("url");
 var qs = require("querystring");
 
-function templateHTML(title, list, body) {
+function templateHTML(title, list, body, control) {
   return `<!DOCTYPE html>
   <html>
     <head>
@@ -13,7 +13,7 @@ function templateHTML(title, list, body) {
     <body>
       <h1><a href="/">WEB</a></h1>
       ${list}
-      <a href="/create">create</a>
+      ${control}
       ${body}
     </body>
   </html>
@@ -44,8 +44,8 @@ var app = http.createServer(function (request, response) {
         var template = templateHTML(
           title,
           list,
-          `<h2>${title}</h2>
-        <p>${description}</p>`
+          `<h2>${title}</h2> <p>${description}</p>`,
+          `<a href="/create">create</a>`
         );
 
         response.writeHead(200);
@@ -62,8 +62,13 @@ var app = http.createServer(function (request, response) {
             var template = templateHTML(
               title,
               list,
-              `<h2>${title}</h2>
-            <p>${description}</p>`
+              `<h2>${title}</h2> <p>${description}</p>`,
+              `<a href="/create">create</a>
+              <a href="/update?id=${title}">update</a>
+              <form action="delete_process" method="post">
+                <input type="hidden" name="id" value="${title}">
+                <input type="submit" value="delete">
+              </form>`
             );
             response.writeHead(200);
             response.end(template);
@@ -78,7 +83,7 @@ var app = http.createServer(function (request, response) {
       var template = templateHTML(
         title,
         list,
-        `<form action="http://localhost:3000/create_process" method="post">
+        `<form action="/create_process" method="post">
           <p><input type="text" name="title" placeholder="title"/></p>
           <p>
             <textarea name="description" placeholder="description"></textarea>
@@ -86,7 +91,8 @@ var app = http.createServer(function (request, response) {
           <p>
             <input type="submit" />
           </p>
-        </form>`
+        </form>`,
+        ``
       );
 
       response.writeHead(200);
@@ -95,9 +101,11 @@ var app = http.createServer(function (request, response) {
   } else if (pathname === "/create_process") {
     var body = "";
     request.on("data", function (data) {
+      //request에 data가 있을 경우 처리 하는 부분
       body += data;
     });
     request.on("end", function () {
+      //data 처리가 다 끝난 경우
       var post = qs.parse(body);
       var title = post.title;
       var description = post.description;
@@ -105,6 +113,71 @@ var app = http.createServer(function (request, response) {
       // fs.writeFile(filename, content, encoding, callback)
       fs.writeFile(`../data/${title}`, description, "utf-8", function (err) {
         response.writeHead(302, { Location: `/?id=${title}` });
+        response.end();
+      });
+    });
+  } else if (pathname === "/update") {
+    fs.readdir("../data", function (error, filelist) {
+      fs.readFile(
+        `../data/${queryData.id}`,
+        "utf8",
+        function (err, description) {
+          var title = queryData.id;
+          var list = templateList(filelist);
+          var template = templateHTML(
+            title,
+            list,
+            `<form action="/update_process" method="post">
+              <input type="hidden" name="id" value="${title}">
+              <p><input type="text" name="title" value="${title}"/></p>
+              <p>
+                <textarea name="description"> ${description}</textarea>
+              </p>
+              <p>
+                <input type="submit" />
+              </p>
+            </form>`,
+            `<a href="/create">create</a>
+            <a href="/update?id=${title}">update</a>`
+          );
+          response.writeHead(200);
+          response.end(template);
+        }
+      );
+    });
+  } else if (pathname === "/update_process") {
+    var body = "";
+    request.on("data", function (data) {
+      //request에 data가 있을 경우 처리 하는 부분
+      body += data;
+    });
+    request.on("end", function () {
+      //data 처리가 다 끝난 경우
+      var post = qs.parse(body);
+      var id = post.id;
+      var title = post.title;
+      var description = post.description;
+
+      fs.rename(`../data/${id}`, `../data/${title}`, function (error) {
+        // fs.writeFile(filename, content, encoding, callback)
+        fs.writeFile(`../data/${title}`, description, "utf-8", function (err) {
+          response.writeHead(302, { Location: `/?id=${title}` });
+          response.end();
+        });
+      });
+    });
+  } else if (pathname === "/delete_process") {
+    var body = "";
+    request.on("data", function (data) {
+      //request에 data가 있을 경우 처리 하는 부분
+      body += data;
+    });
+    request.on("end", function () {
+      //data 처리가 다 끝난 경우
+      var post = qs.parse(body);
+      var id = post.id;
+      fs.unlink(`../data/${id}`, function (error) {
+        response.writeHead(302, { Location: `/` });
         response.end();
       });
     });
