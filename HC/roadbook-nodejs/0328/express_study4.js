c3dd5402feb244707cbbbb25437f8045f0f0f26f
HeonchanKim <khc9274@naver.com>
2022-03-28 23:54:24 +0900

    헌찬 0328

---
+++ HC/roadbook-nodejs/0328/express_study4.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const { reset } = require('nodemon');
+const app = express();
+
+app.get('/', function (req,res,next){
+    res.send('Hello World!');
+    next();
+});
+
+const myLogger = function(req,res,next){
+    console.log('LOGGED');
+    next();
+}
+
+app.use(myLogger);
+
+app.listen(8082);
